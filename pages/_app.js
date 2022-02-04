3eb4cac2b312a1b3d401126cd1e02111333e760b
Einiqorn <98750401+einiqorn@users.noreply.github.com>
2022-02-04 22:31:16 +0100

    Set global behaviors and styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,31 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import AOS from 'aos'
+
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+function TopStonk({ Component }) {
+  const router = useRouter()
+  const location = router.pathname
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 500,
+      easing: 'ease-out-cubic',
+    })
+  })
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'smooth'
+    window.scroll({ top: 0 })
+    document.body.className =
+      'antialiased tracking-tight bg-white font-inter text-slate-800'
+  }, [location.pathname])
+
+  return <Component location={location} />
 }
 
-export default MyApp
+export default TopStonk
