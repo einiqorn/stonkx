a070025886daad439aab38b3bab7a53932fb0942
Einiqorn <98750401+einiqorn@users.noreply.github.com>
2022-02-04 19:11:42 +0100

    Create simple LP

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <p>Top stonk, very nice. Make you so much money.</p>
+  return (
+    <header className="flex items-center justify-center w-screen h-screen bg-slate-900">
+      <div className="text-center">
+        <h1 className="pb-8 text-6xl text-white font-playfair-display">
+          Top Stonk 2022
+        </h1>
+        <p className="text-xl text-white font-inter">
+          Buy my stonk, very nice. Make you so much money.
+        </p>
+      </div>
+    </header>
+  )
 }
