49a0475b7aa8993385ce3252f1f2925ca7d544fa
Einiqorn <98750401+einiqorn@users.noreply.github.com>
2022-02-04 19:51:28 +0100

    Convert from React to Preact

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
   assetPrefix: isProd ? '/stonkx/' : '',
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+    return config
+  },
 }
