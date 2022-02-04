378b29d558bc9f6822665ce7bd106ebc4a3ed3c3
einiqorn <einiqorn@users.noreply.github.com>
2022-02-04 21:32:13 +0000

    deploy: 3eb4cac2b312a1b3d401126cd1e02111333e760b

--- _next/static/w-u9YvPrTmiy_Sq2MnyDD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1d4106ac1c5a3c07.js"],"/_error":["static/chunks/pages/_error-34b1906f689dcf74.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
